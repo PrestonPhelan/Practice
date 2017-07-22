085f184ff790cfd8d8a60648e27ed72d2646ec73
PrestonPhelan <preston.phelan@gmail.com>
2017-07-22 01:36:22 -0700

    Compare binomial samples

--- binom_dist.rb
+++ binom_dist.rb
@@ -1,6 +1,33 @@
 class BinSample
+  attr_reader :size, :success_rate
+
   def initialize(size, success_rate)
     @size = size
     @success_rate = success_rate / 100.0
   end
+
+  def inverse_size
+    1.0 / size
+  end
+end
+
+def compare_samples(sample1, sample2)
+  numerator = sample1.success_rate - sample2.success_rate
+  coefficient = comparison_coefficient(sample1, sample2)
+  denominator = Math.sqrt(
+    coefficient *
+    (1 - coefficient) *
+    (sample1.inverse_size +
+    sample2.inverse_size))
+  numerator / denominator
+end
+
+def comparison_coefficient(sample1, sample2)
+  numerator = comparison_numerator(sample1) + comparison_numerator(sample2)
+  denominator = sample1.size + sample2.size
+  numerator / denominator
+end
+
+def comparison_numerator(sample)
+  sample.size * sample.success_rate
 end
